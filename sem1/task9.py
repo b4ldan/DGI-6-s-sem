fb4ec79b0727ed1adc90bbd71bf0270e8bbb46ea
BogdanL <b4ldanlegotn@gmail.com>
2024-02-10 13:33:01 +0300

    fin

---
+++ sem1/task9.py
@@ -0,0 +1,11 @@
+numbers=['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+print('Вышел зайчик погулять')
+
+print('')
+
+numbers=['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+    print('Вышел зайчик погулять')
