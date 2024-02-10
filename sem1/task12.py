40fb503b43d95dc52d92d0040d7a40ddf87c0366
BogdanL <b4ldanlegotin@gmail.com>
2024-02-10 21:51:29 +0300

    task13

--- sem1/task12.py
+++ sem1/task12.py
@@ -1,5 +1,5 @@
 countdown_str=''
-for i in reversed(range(0,11)):
+for i in reversed(range(11)):
     countdown_str=countdown_str+str(i)+', '
 countdown_str=countdown_str+'поехали!'
 print(countdown_str)
