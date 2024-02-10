d81f72bed6df9cb4a7142979c63e0bbb193a00d3
BogdanL <b4ldanlegotin@gmail.com>
2024-02-10 21:59:48 +0300

    task15

---
+++ sem1/task15.py
@@ -0,0 +1,9 @@
+for messages_count in (0,21):
+    if messages_count == 0:
+        print('У вас нет сообщений')
+    elif messages_count == 1:
+        print('У вас 1 новое сообщение')
+    elif messages_count <= 4:
+        print('У вас',messages_count,'новое сообщение')
+    elif messages_count >4: 
+        print('У вас',messages_count,'новых сообщений')
