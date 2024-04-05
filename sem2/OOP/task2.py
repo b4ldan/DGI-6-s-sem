1d4f12cf5ed3dc2bb3f4f26783c08e7058214025
BogdanL <b4ldanlegotin@gmail.com>
2024-04-05 20:57:21 +0300

    task2(nw)

--- sem2/OOP/task2.py
+++ sem2/OOP/task2.py
@@ -1,9 +1,16 @@
 class Song:
+    
     def __init__(self, title, artist, release_year) -> None:
         self.title = title
         self.artist = artist
         self.release_year = release_year
+
+    def get_into(self):
+        info = {"title":self.title,
+                "artist":self.artist,
+                "release_year":self.release_year}
+        return info
         
 
 some_song= Song('Some song', 'Somebody', 2024)
-print(some_song.title)
+print(some_song.get_info())
