02e085dbe881755d636794785124ff214514e68d
BogdanL <b4ldanlegotin@gmail.com>
2024-04-05 20:39:01 +0300

    task3

--- sem2/data_type/task3.py
+++ sem2/data_type/task3.py
@@ -1,17 +1,17 @@
 def change_list(list):
     import copy
     list=copy.deepcopy(list)
-    list.append('привет!')
-    print('Значение списка после изменения значения =', list)
-    print('Id списка после изменения значения', id(list))
+    list.append('Hello!')
+    print('List meaning after change =', list)
+    print('List ID after change =', id(list))
     print('\n')
 
 my_list = ['PT1,']
-print('Значение списка до вызова функции=', my_list)
-print('Id списка до вызова функции =', id(my_list))
+print('List meaning before =', my_list)
+print('List ID before = ', id(my_list))
 print('\n')
 
 change_list(my_list)
-print('Значение списка после вызова функции', my_list) 
-print('Id списка после вызова функции=', id(my_list))
+print('List meaning after =', my_list)
+print('List ID after = ', id(my_list))
 print('\n')
